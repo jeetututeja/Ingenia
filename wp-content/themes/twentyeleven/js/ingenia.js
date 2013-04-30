1ad95a08f0b1e6718f840550bab7f5121785da00
Juan Diego Gonzales <jd@juandiegogonzales.com>
2013-04-30 02:52:38 -0500

    .gitignore working good

--- wp-content/themes/twentyeleven/js/ingenia.js
+++
@@ -1,6 +0,0 @@
-var beforeDay = ["Feliz","Buen","Sí, es","Whooa,","Yajúa,","Bon","Yay,","Es","Yuju, es"];
-var coeficient = beforeDay.length - 1;
-var randomBeforeDay = Math.round(Math.random()*(coeficient));
-var dayNames = ["domingo","lunes","martes","miércoles","jueves","viernes","sábado"];
-var now = new Date();
-document.write(beforeDay[randomBeforeDay] + " " + dayNames[now.getDay()] + ",");
